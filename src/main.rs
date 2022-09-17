03d454effe6fb9adec2d68aa154907f8f0296317
JoshuaDRose <joshuarose099@gmail.com>
2022-09-17 16:49:08 +1000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;  // Equivalent to using namespace std; in c++
+use std::io;
 
 fn main() {
 
@@ -8,9 +8,17 @@ fn main() {
     let secret_num = rand::thread_rng().gen_range(1, 10);
 
     println!("I\'m thinking of a number between 1 and 10!");
+    println!("The secret number is: {}", secret_num);
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Higher"),
+        Ordering::Higher => println!("Lower"),
+        Ordering::Equal => println!("Correct!"),
+    }
 }
