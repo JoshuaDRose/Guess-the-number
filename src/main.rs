61ade9299b46804dd18ceb055d5d37ef50ce7a96
JoshuaDRose <joshuarose099@gmail.com>
2022-09-17 15:43:31 +1000

    Improve inline comments readability

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io; // Equivalent to using namespace std; in c++
+use std::io;  // Equivalent to using namespace std; in c++
 
 fn main() {
     println!("I\'m thinking of a number between 1 and 10!");
-    let mut guess = String::new(); // new() is (an) assoc-func.
+    let mut guess = String::new();  // new() is (an) assoc-func.
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 }
