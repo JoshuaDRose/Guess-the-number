87146b65700e6472db2682f56297d0a4a034f357
JoshuaDRose <joshuarose099@gmail.com>
2022-09-17 16:36:44 +1000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;  // Equivalent to using namespace std; in c++
 
 fn main() {
-    println!("I\'m thinking of a number between 1 and 10!");
-    // Instance type string
+
     let mut guess = String::new();
     let secret_num = rand::thread_rng().gen_range(1, 10);
-    // Receive user input
+
+    println!("I\'m thinking of a number between 1 and 10!");
+
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
-    // Print out guess
+
     println!("You guessed: {}", guess);
 }
