5e6b91dae53bfdeaec010dba62e37c666039d556
JoshuaDRose <joshuarose099@gmail.com>
2022-09-17 15:55:33 +1000

    Improved comment readability

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::io;  // Equivalent to using namespace std; in c++
 
 fn main() {
     println!("I\'m thinking of a number between 1 and 10!");
-    let mut guess = String::new();  // new() is (an) assoc-func.
+    // Instance type string
+    let mut guess = String::new();
+    // Receive user input
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
+    // Print out guess
+    println!("You guessed: {}", guess);
 }
